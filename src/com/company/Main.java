c7be1bc69eb3e7786d19e2985fdba4ad74cd841b
moazzamsadiq <moazzam@syntaxtechs.com>
2022-10-02 09:33:13 -0400

    t

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+
+package com.company;
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+	// write your code here
+
+        for (int i=0; i<5;i++){
+
+            System.out.println("waow");
+            Thread.sleep(3000);
+        }
+    }
+}
